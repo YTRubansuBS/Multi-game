f35042ef5619a5d2649b332e3ff74f4d69d67737
YTRubansuBS <rubendeclaye@gmail.com>
2026-09-10 21:58:29 +0200

    Load social rooms module in Vercel build

--- build.js
+++ build.js
@@ -1,5 +1,7 @@
 const fs=require('fs');
 if(!fs.existsSync('public')) fs.mkdirSync('public');
-const html=fs.readFileSync('index.html','utf8');
+let html=fs.readFileSync('index.html','utf8');
+if(!html.includes('rooms.js')) html=html.replace('</body>','<script src="/rooms.js"></script></body>');
 fs.writeFileSync('public/index.html',html);
-console.log('CROWN RIFT build OK: public/index.html created.');
+fs.copyFileSync('rooms.js','public/rooms.js');
+console.log('CROWN RIFT build OK: index + social rooms + admin copied.');
